47c647c3acf6ae2b2a6fae5be715165b9d748a62
to-4 <to-4@example.com>
2025-10-01 13:05:59 +0900

    feat: ユニットテスト環境の実装
    - create test DB on MySQL
    - modify database.php
    - create .env.testing
    - modify phpunit.xml

---
+++ src/tests/Feature/HelloTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
+// use Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase; // ← LaravelのではなくPHPUnitのTestCase
+
+
+class HelloTest extends TestCase
+{
+    #[Test]
+    public function hello_world(): void
+    {
+        $this->assertTrue(true); // Assert that true is true
+
+        $arr = [];
+        $this->assertEmpty($arr); // Assert that the array is empty
+
+        $txt = "Hello World";
+        $this->assertEquals('Hello World', $txt); // Assert that the text equals "Hello World"
+
+        $n = random_int(0, 100);
+        $this->assertLessThan(100, $n); // Assert that the random number is less than 100
+    }
+}
